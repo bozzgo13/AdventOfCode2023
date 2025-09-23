f6c6c5cbc66cf5a20019b46d194f4f82de18fcfe
Boštjan Tučič <bostjan.tucic@comtradegaming.com>
2025-09-23 12:51:57 +0200

    Added catching FileNotFoundException. Renaming GetDigit into GetFirstDigit. Removed unnecessary method parameter

--- Day 1/1/adventofcode_2023_1/Program.cs
+++ Day 1/1/adventofcode_2023_1/Program.cs
@@ -1,12 +1,18 @@
 ﻿
-string fileName = "input.txt";
+
+//input file in Day1 folder (exe in net6.0 folder. Go backward - Day 1\1\adventofcode_2023_1\bin\Debug\net6.0)
+string fileName = "..\\..\\..\\..\\..\\input.txt";
 
 //result 55130
 
 int endResult = 0;
-var lines = File.ReadLines(fileName);
-foreach (var lineOriginal in lines)
+IEnumerable<string> lines = null;
+
+try
 {
+  lines = File.ReadLines(fileName);
+  foreach (var lineOriginal in lines)
+  {
     var line = lineOriginal.ToLower();
     var lineReverse = string.Concat(line.Reverse());
 
@@ -14,21 +20,28 @@ foreach (var lineOriginal in lines)
     string numberAsString = string.Empty;
 
 
-    chars[0] = GetDigit(line, false);
-    chars[1] = GetDigit(lineReverse, true);
+    chars[0] = GetFirstDigit(line);
+    chars[1] = GetFirstDigit(lineReverse);
 
     numberAsString = new string(chars);
 
     int result = System.Convert.ToInt32(numberAsString);
     endResult += result;
 
+  }
 }
+catch (System.IO.FileNotFoundException fnfE)
+{
+  Console.WriteLine($"File not found : {fnfE.Message}");
+  return;
+}
+
 
 
 Console.WriteLine($"result : {endResult}");
 
 
-char GetDigit(string line, bool revert)
+char GetFirstDigit(string line)
 {
    
     for (int i = 0; i < line.Length; i++)
